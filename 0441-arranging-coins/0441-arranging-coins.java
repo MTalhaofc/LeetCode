c787ec17fe80ae759e90eddf394bba47641051b8
Muhammad Talha <talha0750@gmail.com>
2024-09-16 16:04:09 +0500

    Time: 9 ms (11.42%), Space: 40.5 MB (94.27%) - LeetHub

---
+++ 0441-arranging-coins/0441-arranging-coins.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int arrangeCoins(int n) {
+        int i = 0; // Initialize the row counter
+        while (n > 0 && n > i) { // Continue until there aren't enough coins to form the next row
+            i++; // Increment the row number
+            n = n - i; // Subtract the current row number of coins from n
+        }
+        return i; // Return the number of complete rows
+    }
+}
