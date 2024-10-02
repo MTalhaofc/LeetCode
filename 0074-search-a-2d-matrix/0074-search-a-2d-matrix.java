a26447392e76de01e471f7899a00b27e5ab1d0b1
Muhammad Talha <talha0750@gmail.com>
2024-10-02 23:09:07 +0500

    Time: 0 ms (100%), Space: 42.3 MB (28.46%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,23 +1,23 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        // Check if the matrix is empty
-        if (matrix.length == 0 || matrix[0].length == 0) {
+          if (matrix.length == 0 || matrix[0].length == 0) {
             return false;
         }
-
+        
         int row = 0;
-        int col = matrix[0].length - 1; // Start from the top-right corner
-
-        while (row < matrix.length && col >= 0) {
-            if (matrix[row][col] == target) {
-                return true; // Found the target
-            } else if (matrix[row][col] > target) {
-                col--; // Move left
-            } else {
-                row++; // Move down
-            }
+        int col = matrix[0].length-1;
+       
+       
+        while(row < matrix.length && col >=0){
+        if(matrix[row][col]==target){
+            return true;
         }
-
-        return false; // Target not found
-    }
-}
+        else if(matrix[row][col]>target){
+            col--;
+        }
+        else if(matrix[row][col]<target){
+            row++;
+        }
+        }   
+                return false;
+    }       }
