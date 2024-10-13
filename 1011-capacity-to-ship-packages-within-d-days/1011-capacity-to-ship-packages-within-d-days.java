9cad7562ec48e730fe1c3c56957060006c02b617
Muhammad Talha <talha0750@gmail.com>
2024-10-13 15:48:05 +0500

    Time: 10 ms (91.72%), Space: 47 MB (73.05%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -1,37 +1,35 @@
 class Solution {
     public int shipWithinDays(int[] nums, int days) {
-        int capacity = maxweight(nums);  // Start with the heaviest item as the minimum capacity
-        int daysCounter = 1;  // Start with at least 1 day
-        int currentWeight = 0;  // Track the current total weight for a single day
-
-        for (int i = 0; i < nums.length; i++) {
-            // If adding the next item exceeds the capacity, ship it the next day
-            if (currentWeight + nums[i] > capacity) {
-                daysCounter++;  // Start a new day
-                currentWeight = 0;  // Reset the current weight for the new day
-
-                // If we've exceeded the allowed number of days, increment capacity
-                if (daysCounter > days) {
-                    capacity++;  // Increase the capacity to try again
-                    daysCounter = 1;  // Reset the day counter
-                    currentWeight = 0;  // Reset weight tracking
-                    i = -1;  // Restart the loop
-                }
-            }
-            // Add the item to the current day's shipment
-            currentWeight += nums[i];
+    int min = 0;
+    int max = 0;
+    for(int i =0; i<nums.length; i++){
+        if(nums[i]>min){
+            min = nums[i];
         }
-
-        return capacity;  // Return the final valid capacity
+        max+=nums[i];
     }
-
-    public int maxweight(int[] nums) {
-        int max = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] > max) {
-                max = nums[i];
-            }
+    
+    while(min <max){
+        int edays = 1;
+    int sum = 0;
+        int mid = min + (max-min)/2;
+        for(int i = 0; i<nums.length; i++){
+        if(sum  + nums[i] > mid){
+            edays++;
+            sum = 0;
         }
-        return max;
+        sum+=nums[i];
+        }
+    if(edays > days){
+        min = mid+1;
     }
-}
+    else{
+        max = mid;
+    }
+    }
+   
+   
+   return min;
+   
+    }
+    }
