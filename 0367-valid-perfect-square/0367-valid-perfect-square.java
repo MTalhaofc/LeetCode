65f651d130654cf88b1999d647eb04c7ad9fd936
Muhammad Talha <talha0750@gmail.com>
2024-11-13 23:20:01 +0500

    Time: 0 ms (100%), Space: 40.2 MB (56.02%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.java
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -1,23 +1,26 @@
 class Solution {
     public boolean isPerfectSquare(int num) {
-        if(num==1){
+        long ans = mysqrt(num);
+        if (ans * ans == num) {
             return true;
+        } else {
+            return false;
         }
-        else{
-        int start =0;
-        int end = num;
-        while(start < end){
-            int mid=start+ (end-start)/2;
-            if(mid*mid == num){
-                return true;
+    }
+    
+    public long mysqrt(int num) {
+        long start = 0;
+        long end = num;
+        while (start <= end) {
+            long mid = start + (end - start) / 2;
+            if (mid * mid == num) {
+                return mid;
+            } else if (mid * mid > num) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
             }
-            else if(mid*mid>num){
-                end = mid;
-            }
-            else{
-                start = mid+1;
-            }
-        }}
-        return false;
+        }
+        return Math.round(end);
     }
 }
